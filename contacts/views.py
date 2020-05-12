f98aac5aca0663292fdc87c4d310603b07f6ca20
Nathon Reed <nrwebdev00@gmail.com>
2020-05-12 09:16:32 -0700

    finshed project ready fro depoly

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -12,8 +13,22 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+
+        if request.user.is_authenticated:
+          user_id = request.user.id
+          has_sent_contacted_already = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+          if has_sent_contacted_already:
+              messages.error(request, 'You have already made an inquiry for this listing')
+              return redirect('/listings/'+listing_id)  
         
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+        send_mail(
+            'Property Listing Inquiry for ' + listing,
+            'There has been inquiry for ' + listing + ' please sign in to the admin panel to view.',
+            'reproject.email@gmail.com',
+            [realtor_email, 'nrwebdev00@gmail.com'],
+            fail_silently=False
+        )
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
